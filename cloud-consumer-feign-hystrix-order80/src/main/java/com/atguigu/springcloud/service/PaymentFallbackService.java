5373b2b2c486c0d1403025c1921eedb1adb8d7bd
gm-hacker <gm-hacker@qq.com>
2020-08-21 19:11:23 +0800

    10、Hystrix相关

---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/service/PaymentFallbackService.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud.service;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author gm-hacker
+ * @create 2020-08-21 17:01
+ */
+@Component
+public class PaymentFallbackService implements PaymentHystrixService {
+    @Override
+    public String paymentInfo_OK(Integer id) {
+        return "-----PaymentFallbackService fall back-paymentInfo_OK ,o(╥﹏╥)o";
+    }
+
+    @Override
+    public String paymentInfo_TimeOut(Integer id) {
+        return "-----PaymentFallbackService fall back-paymentInfo_TimeOut ,o(╥﹏╥)o";
+    }
+}
